d7bf263ce1af0e7fba95590ffa82cf54b87e4ca2
Andrey <dr.nakonechniy@yandex.ru>
2023-11-22 02:39:20 +0300

    fix code style

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -13,26 +13,26 @@ import java.util.Map;
 @RestController
 @Slf4j
 public class FilmController {
-    private Map<Integer,Film> films = new HashMap<>();
+    private Map<Integer, Film> films = new HashMap<>();
 
     @GetMapping("/films")
-    public List<Film> showAllFilms(){
+    public List<Film> showAllFilms() {
         return new ArrayList<>(films.values());
     }
 
     @PostMapping("/film")
-    public Film createFilm(@RequestBody Film film){
+    public Film createFilm(@RequestBody Film film) {
         log.info("Получен запрос POST /film.");
         FilmValidation.validate(film);
-        films.put(film.getId(),film);
+        films.put(film.getId(), film);
         return film;
     }
 
     @PutMapping("/film")
-    public Film updateFilm(@RequestBody Film film){
+    public Film updateFilm(@RequestBody Film film) {
         log.info("Получен запрос PUT /film.");
         FilmValidation.validate(film);
-        films.put(film.getId(),film);
+        films.put(film.getId(), film);
         return film;
     }
 }
