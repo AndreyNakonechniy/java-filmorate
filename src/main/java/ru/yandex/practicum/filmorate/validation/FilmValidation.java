2c63630f15f83a5c9f057ea3f173b8052d3e10fe
Andrey <dr.nakonechniy@yandex.ru>
2023-11-22 02:48:55 +0300

    delete unused import

--- src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.validation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpStatus;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
